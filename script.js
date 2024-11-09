6b2898c0981b2623fa296edb4d144e0228e767e6
dannygallows <43937866+dannygallows@users.noreply.github.com>
2024-11-09 19:57:04 +0100

    Add button click logic, populating display

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const display = document.querySelector(".display");
 let x = 0;
 let y = 0;
 let operator = "";
+let displayArray = [];
 
 function add(a, b) {
     return a + b;
@@ -28,6 +29,52 @@ function operate (a, b, operator) {
         case "/":
             return divide(a,b);
     } 
+}
+
+let buttons = document.querySelectorAll("button");
+buttons.forEach( (button) => {
+    button.addEventListener("click", () => {
+        switch (button.textContent) {
+            case "AC":
+                console.log("ok");
+                break;
+            case "←":
+                console.log("ok");
+                break;
+            case "%":
+                console.log("ok");
+                break;
+            case "/":
+                console.log("ok");
+                break;
+            case "*":
+                console.log("ok");
+                break;
+            case "-":
+                console.log("ok");
+                break;
+            case "+":
+                console.log("ok");
+                break;
+            case ",":
+                console.log("ok");
+                break;
+            default:
+                displayPopulate(button.textContent);
+        }
+    })
+})
+
+function displayPopulate (number) {
+    displayArray.push(number);
+    
+    // probably should try to find out how to keep separate every three numbers
+    // for (let i = 0; i < displayArray.length; i++) {
+    //     if (i % 3 == 0) displayArray.push(" ");
+    // }
+
+    output = displayArray.join("");
+    display.textContent = output;
 }
 
 
